e775603de2cf69211863161aeaf94ba069b3f876
Ngokal <69497103+Ngokal@users.noreply.github.com>
2021-01-02 17:06:53 +0300

    Add files via upload

---
+++ bitcoin_odev/coin.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bitcoin_odev
+{
+    class coin
+    {
+        public string[] info;
+
+        public coin(string[] info_)
+        {
+           info = info_;
+        }
+
+        public void yazdir()        
+        {
+            for (int x = 0; x < info.Length; x++)
+            {
+                Console.Write(info[x] + "   ");
+
+            }
+            Console.Write("\n");
+        }
+
+    }
+}
